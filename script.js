982578299c4df4b5623adcdad180ee9deb2d194c
Mohd Amir <you@example.commohdamir1315@gmail.com>
2025-04-19 15:21:42 +0800

    script created
    5 questions and answers are added to the script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: "What is the game genre that focuses on multiplayer element?",
+        answers: [
+            {text: "Visual Novel(VN)", correct: false},
+            {text: "Massively Multiplayer Online Role-Playing Game(MMORPG)", correct: true},
+            {text: "Tactical Role-Plaing Game(TRPG)", correct: false},
+            {text: "Role-Playing Game(RPG)", correct: false},
+        ]
+    },
+    {
+        question: "What is the game genre that falls into TRPG category?",
+        answers: [
+            {text: "Final Fantasy Tactics", correct: true},
+            {text: "Final Fantasy X HD Remaster", correct: false},
+            {text: "Final Fantasy XV Royal Edition", correct: false},
+            {text: "Theathrythm Final Bar", correct: false},
+        ]
+    },
+    {
+        question: "Which of the games below tells the tragic story of a mentally ill serial killer?",
+        answers: [
+            {text: "Zero Escape 9 Hours, 9 Persons, 9 Doors", correct: false},
+            {text: "Phoenix Wright Ace Attorney - Dual Destinies", correct: false},
+            {text: "13 Sentinels Aegis Rim", correct: false},
+            {text: "Famicom Detective Club: Emio - The Smiling Man", correct: true},
+        ]
+    },
+    {
+        question: "What is the JRPG/MMORPG game's class that primarily used for tanking?",
+        answers: [
+            {text: "Black Mage", correct: false},
+            {text: "Samurai", correct: flase},
+            {text: "Paladin", correct: true},
+            {text: "Dragoon", correct: false},
+        ]
+    },
+    {
+        question: "All of the games listed below are turn-based JRPGs, except for?",
+        answers: [
+            {text: "Unicorn Overlord", correct: true},
+            {text: "Octopath Travler II", correct: false},
+            {text: "Persona 5 Royal", correct: false},
+            {text: "Kingdom Hearts Chain of Memories", correct: false},
+        ]
+    }
+];
